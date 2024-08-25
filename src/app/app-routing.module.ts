c39be8bc4c3bcb2df2f8ead90edc4a5dba619a8f
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-08-25 21:28:52 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './homepage/login/login.component';
+import { RegisterComponent } from './homepage/register/register.component';
+import { WelcomeComponent } from './homepage/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    children: [
+      { path: '', component: WelcomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      // Altre rotte possono essere aggiunte qui
+      // { path: '', redirectTo: '/login', pathMatch: 'full' }, // Reindirizza alla pagina di login di default
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './homepage/login/login.component';
+import { RegisterComponent } from './homepage/register/register.component';
+import { WelcomeComponent } from './homepage/welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomepageComponent, LoginComponent, RegisterComponent, WelcomeComponent],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
